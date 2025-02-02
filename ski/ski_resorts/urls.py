934ba575b3b33430b1b93bcf04b790ad0b398674
ankoure <andrew@koure.org>
2025-02-02 03:30:48 +0000

    add basic map

--- ski/ski_resorts/urls.py
+++ ski/ski_resorts/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
+from ski_resorts.views import MapView
+
 
 urlpatterns = [
     path('ski_resorts/', views.landingpage, name='landingpage'),
+    path("map/", MapView.as_view()),
+
 ]
--- ski/ski_resorts/views.py
+++ ski/ski_resorts/views.py
@@ -1,6 +1,26 @@
 from django.http import HttpResponse
 from django.template import loader
+from ski_resorts.models import Resort
+import json
+from django.core.serializers import serialize
+from django.views.generic import ListView
 
 def landingpage(request):
   template = loader.get_template('myfirst.html')
   return HttpResponse(template.render())
+
+class MapView(ListView):
+    context_object_name = "markers"
+    model = Resort
+    template_name = "map.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(
+            **kwargs
+        )
+        context["markers"] = json.loads(
+            serialize(
+                "geojson", context["markers"]
+            )
+        )
+        return context
